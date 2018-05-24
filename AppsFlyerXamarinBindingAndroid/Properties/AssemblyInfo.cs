8a2b4e0c33b628fae918a1c1c773cb44fdc11ba4
Unknown <benjamin@appsflyer.com>
2018-05-24 16:24:33 +0300

    AssemblyVersionUpdate

--- AppsFlyerXamarinBindingAndroid/Properties/AssemblyInfo.cs
+++ AppsFlyerXamarinBindingAndroid/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using Android.App;
 [assembly: AssemblyTitle("AppsFlyerXamarinBindingAndroid")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AppsFlyer")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("${AuthorCopyright}")]
+[assembly: AssemblyCopyright("AppsFlyer Ltd.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,7 +18,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.4.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
