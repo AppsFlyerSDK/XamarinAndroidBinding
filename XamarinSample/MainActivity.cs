efcfb5bec526ef1b91e09b821c3b292dad450122
af-vs <vitaly.sokolov@appsflyer.com>
2021-01-11 16:15:30 +0200

    Update SDK to 6.1.3

--- XamarinSample/MainActivity.cs
+++ XamarinSample/MainActivity.cs
@@ -47,7 +47,7 @@ namespace XamarinSample
             gcdTextView = FindViewById<AppCompatTextView>(Resource.Id.gcd_text_view);
             oaoaTextView = FindViewById<AppCompatTextView>(Resource.Id.oaoa_text_view);
 
-
+            AppsFlyerLib.Instance.AddPushNotificationDeepLinkPath(new string[] { "key1", "key2" });
             AppsFlyerLib.Instance.SetLogLevel(AFLogger.LogLevel.Verbose); // Enable verbose logs for debugging
             AppsFlyerLib.Instance.Init("4UGrDF4vFvPLbHq5bXtCza", new AppsFlyerConversionDelegate(this), Application);
             AppsFlyerLib.Instance.SetAppInviteOneLink("E2bM"); // Replace with OneLink ID from your AppsFlyer account
